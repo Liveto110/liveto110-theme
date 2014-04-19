1cea8beb0a19b93cf402458643371794a4f19164
Praveen Ramachandran <praveen.menon@innoplexus.com>
2014-04-19 19:43:07 +0530

    Layout grids on common template files.

--- index.php
+++ index.php
@@ -13,34 +13,43 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="content" class="site-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8">
+				<div id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
+						<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
+							<?php /* Start the Loop */ ?>
+							<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+								<?php
+									/* Include the Post-Format-specific template for the content.
+									* If you want to override this in a child theme, then include a file
+									* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+									*/
+									get_template_part( 'content', get_post_format() );
+								?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+							<?php endwhile; ?>
 
-			<?php endwhile; ?>
+							<?php liveto110_paging_nav(); ?>
 
-			<?php liveto110_paging_nav(); ?>
+						<?php else : ?>
 
-		<?php else : ?>
+							<?php get_template_part( 'content', 'none' ); ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+						<?php endif; ?>
+					</main><!-- #main -->
+				</div><!-- #primary -->
+			</div><!-- /.col-sm-8 -->
 
-		<?php endif; ?>
+			<div class="hidden-phone col-sm-4">
+				<?php get_sidebar(); ?>
+			</div><!-- /.hidden-phone .col-sm-4 -->
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+</div><!-- /#content -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- search.php
+++ search.php
@@ -7,32 +7,43 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="content" class="site-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8">
+				<section id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+					<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'liveto110' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+						<header class="page-header">
+							<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'liveto110' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+						</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+						<?php /* Start the Loop */ ?>
+						<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'search' ); ?>
+							<?php get_template_part( 'content', 'search' ); ?>
 
-			<?php endwhile; ?>
+						<?php endwhile; ?>
 
-			<?php liveto110_paging_nav(); ?>
+						<?php liveto110_paging_nav(); ?>
 
-		<?php else : ?>
+					<?php else : ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+						<?php get_template_part( 'content', 'none' ); ?>
 
-		<?php endif; ?>
+					<?php endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+					</main><!-- #main -->
+				</section><!-- #primary -->
+			</div><!-- /.col-sm-8 -->
 
-<?php get_sidebar(); ?>
+			<div class="hidden-phone col-sm-4">
+				<?php get_sidebar(); ?>
+			</div><!-- /.hidden-phone .col-sm-4 -->
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+</div><!-- /#content -->
+
 <?php get_footer(); ?>
