eaa957cc26a6f051dab8b8f20f3ab7618854eccf
Praveen Ramachandran <praveen.menon@innoplexus.com>
2014-04-26 00:31:38 +0530

    Sidebar edits

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
-		<span class="screen-reader-text">Search for:</span>
+		<span class="screen-reader-text sr-only">Search for:</span>
 		<input type="search" class="search-field form-control" placeholder="Search <?php bloginfo('name'); ?>" value="" name="s" title="Search for:" />
 	</label>
 	<button class="search-submit btn btn-link" type="button"><i class="fa fa-search"></i></button>
