288c5ff29ae951fc18b24ebf46ecd50ce9ce24a0
Praveen Ramachandran <praveen.menon@innoplexus.com>
2014-04-26 00:11:46 +0530

    blog-index-page

--- index.php
+++ index.php
@@ -25,45 +25,49 @@ get_header(); ?>
 							<?php while ( have_posts() ) : the_post(); ?>
 
 								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<div class="row">
 									<?php if ('' == has_post_thumbnail()) { ?>
 
-										<?php $wrapper_class = 'unfloated'; ?>
+										<?php $wrapper_class = 'col-xs-12'; ?>
 
 									<?php } else { ?>
 
-										<div class="image-wrap">
-											<?php 
-												the_post_thumbnail();
-												$wrapper_class = 'floated';
-											?>
-										</div><!-- /.image-wrap -->
+										<div class="col-xs-4">
+											<div class="image-wrap">
+												<?php
+													the_post_thumbnail('blog-index');
+													$wrapper_class = 'col-xs-8';
+												?>
+											</div><!-- /.image-wrap -->
+										</div>
 
 									<?php } ?>
 
-									<div class="content-wrap <?php echo $wrapper_class; ?>">
-										<header class="entry-header">
-											<?php
-												if ( is_single() ) :
-													the_title( '<h1 class="entry-title">', '</h1>' );
-												else :
-													the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
-												endif;
-											?>
-
-											<div class="entry-meta">
+									<div class="<?php echo $wrapper_class; ?>">
+										<div class="content-wrap">
+											<header class="entry-header">
 												<?php
-													if ( 'post' == get_post_type() )
-														liveto110_posted_on();
-
-													edit_post_link( __( 'Edit', 'liveto110' ), '<span class="edit-link">', '</span>' );
+													if ( is_single() ) :
+														the_title( '<h1 class="entry-title">', '</h1>' );
+													else :
+														the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+													endif;
 												?>
-											</div><!-- .entry-meta -->
-										</header><!-- .entry-header -->
 
-										<div class="entry-summary">
-											<?php the_excerpt(); ?>
-										</div><!-- .entry-summary -->
+												<div class="entry-meta">
+													<?php
+														if ( 'post' == get_post_type() )
+															liveto110_posted_on();
+													?>
+												</div><!-- .entry-meta -->
+											</header><!-- .entry-header -->
+
+											<div class="entry-summary">
+												<?php the_excerpt(); ?>
+											</div><!-- .entry-summary -->
+										</div><!-- /.content-wrap -->
 									</div><!-- /.content-wrap -->
+									</div><!-- /.row -->
 								</article><!-- #post-## -->
 
 							<?php endwhile; ?>
