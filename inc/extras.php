524c6d3fb6de237073f4081cbd7bdbef5dfab166
Praveen Ramachandran <praveen.menon@innoplexus.com>
2014-04-26 17:46:19 +0530

    Typo?

--- inc/extras.php
+++ inc/extras.php
@@ -103,7 +103,6 @@ function custom_excerpt_length( $length ) {
 	return 20;
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
-<<<<<<< HEAD
 
 /*===================================================================================
  * Add Author Links
