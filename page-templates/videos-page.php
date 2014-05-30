62e86dba15ff8d36b0eb767ae7cfe53408993287
Praveen Ramachandran <praveen.menon@innoplexus.com>
2014-05-30 15:10:34 +0530

    Asana => Videos Page --> Mobile Screens

--- page-templates/videos-page.php
+++ page-templates/videos-page.php
@@ -30,6 +30,12 @@ get_header(); ?>
 									<div class="slides">
 										<?php while ( $videos->have_posts()) : $videos->the_post(); ?>
 											<article <?php post_class( 'video-post' ); ?>>
+												<div class="button-wrap">
+													<button class="btn btn-primary" data-toggle="modal" data-target="#video-modal-<?php the_ID(); ?>">
+														<i class="fa fa-play"></i>
+													</button>
+												</div><!-- /.button-wrap -->
+												
 												<div class="image-wrap">
 													<?php
 														$slide_img = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full-width-slide' );
@@ -44,16 +50,14 @@ get_header(); ?>
 													<header class="video-header">
 														<h1 class="video-title h4"><?php the_title(); ?></h1><!-- /.video-title -->
 													</header><!-- /.video-header -->
-
-													<?php the_content(); ?>
 												</div><!-- /.video-content -->
 											</article><!-- /.video-post -->
 										<?php endwhile; ?>
+										<?php wp_reset_postdata(); ?>
 									</div><!-- /.slides -->
 								</div><!-- /.flexslider -->
 							</div><!-- /.videos-wrap -->
-						<?php endif; ?>
-						<?php wp_reset_postdata(); ?>
+						<?php endif; ?>						
 					</main><!-- #main -->
 				</div><!-- #primary -->
 			</div><!-- /.container -->
@@ -61,5 +65,37 @@ get_header(); ?>
 		
 	<?php endwhile; ?>
 </div><!-- /#videos-page-template -->
+
+<div class="video-modals">
+	<?php
+		$args = array(
+			'post_type' => 'video',
+			'posts_per_page' => -1
+		);
+		$videos = new WP_Query( $args );
+	?>
+	<?php if ( $videos->have_posts()) : ?>
+		<!-- Modals START -->
+		<?php while ( $videos->have_posts()) : $videos->the_post(); ?>
+			<div class="modal fade" id="video-modal-<?php the_ID(); ?>" tabindex="-1" role="dialog" aria-labelledby="video-modal-label" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+
+							<h4 class="modal-title" id="myModalLabel"><?php the_title(); ?></h4>
+						</div>
+
+						<div class="modal-body">
+							<?php the_content(); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+		<!-- Modals END -->
+	<?php endif; ?>	
+</div><!-- /.video-modals -->
 
 <?php get_footer(); ?>
