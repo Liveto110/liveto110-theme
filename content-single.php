5377a95b852cd2a5b8b3b8f4381a6787f174be68
Praveen Ramachandran <praveen.menon@innoplexus.com>
2014-06-04 14:48:41 +0530

    Homepage section titles --> bold

--- content-single.php
+++ content-single.php
@@ -31,36 +31,13 @@
 
 	<footer class="entry-footer">
 		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'liveto110' ) );
-
-			/* translators: used between list items, there is a space after the comma */
 			$tag_list = get_the_tag_list( '', __( ', ', 'liveto110' ) );
 
-			if ( ! liveto110_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'liveto110' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'liveto110' );
-				}
+			if ( '' != $tag_list ) {
+				$meta_text = __( 'This article was filed in %1$s.', 'liveto110' );
+			}
 
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'liveto110' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'liveto110' );
-				}
-
-			} // end check for categories on this blog
-
-			printf(
-				$meta_text,
-				$category_list,
-				$tag_list,
-				get_permalink()
-			);
+			printf( $meta_text, $tag_list );
 		?>
 
 		<?php edit_post_link( __( 'Edit', 'liveto110' ), '<span class="edit-link">', '</span>' ); ?>
