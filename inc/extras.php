7240a63232f0be8e263613963f3b6f7bf89e8186
Patrik <worldservice415@gmail.com>
2014-06-12 12:52:24 +0200

    by Patrik

--- inc/extras.php
+++ inc/extras.php
@@ -175,3 +175,6 @@ remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 3
 
 // Remove WooCommerce Breadcrumb
 remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0);
+
+// Remove WooCommerce Result Count
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
