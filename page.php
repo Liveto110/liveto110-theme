2614d4c66445d1aba286c2b1d10795e6690e3fd9
Patrik <worldservice415@gmail.com>
2014-06-20 13:19:27 +0200

    Patrik

--- page.php
+++ page.php
@@ -14,6 +14,9 @@ get_header(); ?>
 
 <div id="content" class="site-content">
 	<div class="container">
+        <header class="page-header">
+            <h1 class="page-title"><?php the_title(); ?></h1>
+        </header>
 		<div class="row">
 			<div class="col-sm-8">
 				<div id="primary" class="content-area">
