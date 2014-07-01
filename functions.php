b08e6d0a5ed2208437e8d2a7c69c8047b8992858
Praveen Ramachandran <praveen.menon@gmail.com>
2014-07-02 03:31:51 +0530

    Remove the word Video from the page.

--- functions.php
+++ functions.php
@@ -128,8 +128,8 @@ function liveto110_widgets_init() {
 		'description' 	=> 'Add the YouTube Channel Gallery widget here.',
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<header class="section-header"><h1 class="widget-title">',
-		'after_title'   => '</h1></header><div class="widget-content">',
+		'before_title'  => '<h1 class="widget-title sr-only">',
+		'after_title'   => '</h1><div class="widget-content">',
 	));
 }
 add_action( 'widgets_init', 'liveto110_widgets_init' );
--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -2,8 +2,6 @@
 /**
  * The Template for displaying all single products.
  *
- * Override this template by copying it to yourtheme/woocommerce/single-product.php
- *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
  * @version     1.6.4
